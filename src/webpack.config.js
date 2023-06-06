5f81912be6a77433420a4426277528869c55afb0
dielawn <dielawnmerkill@gmail.com>
2023-06-05 19:00:15 -0600

    recipe module, ingredient module, recipeIngredient module

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    script: 'script.js',
-    recipes: 'recipes.js',
-    ingredients: 'ingredients.js',
-    recipeIngredients: 'recipeIngredients.js'
+    script: './script.js',
+    recipes: './recipes.js',
+    ingredients: './ingredients.js',
+    recipeIngredients: './recipeIngredients.js'
   },
   output: {
     filename: '[name].bundle.js',
